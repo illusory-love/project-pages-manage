3ddc2a8e8b96d863acb6f52980d459d0de339bff
狐狸的恋人 <illusory_love@aliyun.com>
2017-12-22 00:11:14 +0800

    小程序模版添加对app.json的处理

--- templates/defaults/xcx/index.js
+++ templates/defaults/xcx/index.js
@@ -1,10 +1,29 @@
+const path = require('path')
+const fs = require('fs-extra')
+const writeJson = require('write-json')
+
 exports.onBefore = async (cmd, modulePath, targetPath) => {
 	// console.log('onBefore:', cmd, modulePath, targetPath)
 	// return false
 }
 
 exports.onAfter = async (cmd, modulePath, targetPath) => {
-	console.log('onAfter:', cmd, modulePath, targetPath)
+	// console.log('onAfter:', cmd, modulePath, targetPath)
 	// 创建完成后更新 app.json 配制
-	
+	// 获取 app.json 配制文件
+	const cwd     = process.cwd()
+	const appPath = path.join(cwd, 'app.json')
+	// 判断此配制文件是是否存在
+	if (fs.pathExistsSync(appPath)){
+		let pagesJson = require(appPath)
+		let pages     = pagesJson.pages
+		let route     = targetPath.replace(cwd, '').replace(/\\/g, '/').replace(/^\//, '').split('/')
+		// 转换成app.json中的路由
+		route = route.concat(route.slice(-1)).join('/')
+		// 判断当前路由是否已经存在了
+		if (!pages.includes(route)){
+			pages.push(route)
+			writeJson.sync(appPath, pagesJson)
+		}
+	}
 }
